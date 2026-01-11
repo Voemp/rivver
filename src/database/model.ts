3a9d851769eaebdf2ee982c24a50cf26d2b46e55
Voemp <Hoshinakiyoru@163.com>
2026-01-11 19:55:49 +0800

    ♻️ refactor: 补全项目

---
+++ src/database/model.ts
@@ -0,0 +1,21 @@
+import { createInsertSchema, createSelectSchema } from 'drizzle-typebox'
+import { t } from 'elysia'
+import { table } from './schema'
+import { spreads } from './utils'
+
+export const db = {
+  insert: spreads({
+    user: table.user,
+    feed: createInsertSchema(table.feed, {
+      url: t.String({ format: 'uri' }),
+    }),
+    subscription: table.subscription,
+  }, 'insert'),
+  select: spreads({
+    user: table.user,
+    feed: createSelectSchema(table.feed, {
+      url: t.String({ format: 'uri' }),
+    }),
+    subscription: table.subscription,
+  }, 'select'),
+} as const
